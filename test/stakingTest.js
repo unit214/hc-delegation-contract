dcc14a8b4c3325647968d2f0912dd5f42a3fd27b
Philipp <github@piwo.space>
2024-12-18 10:14:28 +0100

    chore: update nikitas version and hc main contracts

--- test/stakingTest.js
+++ test/stakingTest.js
@@ -2,12 +2,15 @@ const { assert } = require("chai");
 const { utils } = require("@aeternity/aeproject");
 const chaiAsPromised = require("chai-as-promised");
 const chai = require("chai");
+const { generateKeyPair } = require("@aeternity/aepp-sdk");
+const util = require("util");
 
 chai.use(chaiAsPromised);
 
 const HC_ELECTION_CONTRACT_SOURCE = "./contracts/HCElection.aes";
 const MAIN_STAKING_CONTRACT_SOURCE = "./contracts/MainStaking.aes";
-const DELEGATED_STAKING_CONTRACT_SOURCE = "./contracts/DelegatedStaking.aes";
+const DELEGATED_STAKING_CONTRACT_SOURCE =
+  "./contracts/NikitaDelegatedStaking.aes";
 
 BigInt.prototype.toJSON = function () {
   return this.toString();
@@ -16,6 +19,7 @@ BigInt.prototype.toJSON = function () {
 describe("StakingContract", () => {
   let aeSdk;
   let hcElection, mainStaking, delegatedStaking;
+  const validator = generateKeyPair();
 
   async function getEpochInfo() {
     return hcElection.epoch_info().then((res) => res.decodedResult);
@@ -48,7 +52,7 @@ describe("StakingContract", () => {
         .replace("contract MainStaking", "main contract MainStaking"),
       fileSystem: utils.getFilesystem(MAIN_STAKING_CONTRACT_SOURCE),
     });
-    await mainStaking.init(10, 10, 0, 0, 0, 0);
+    await mainStaking.init(10);
 
     hcElection = await aeSdk.initializeContract({
       sourceCode: utils
@@ -66,8 +70,13 @@ describe("StakingContract", () => {
       fileSystem: utils.getFilesystem(DELEGATED_STAKING_CONTRACT_SOURCE),
     });
     await delegatedStaking.init(
+      validator.publicKey,
       mainStaking.$options.address,
-      hcElection.$options.address,
+      10,
+      100,
+      10,
+      0,
+      { amount: 1000 },
     );
 
     await utils.createSnapshot(aeSdk);
@@ -87,30 +96,31 @@ describe("StakingContract", () => {
     assert.equal(getAfter.decodedResult, 2n);
   });
 
-  it("DelegatedStaking: register_validator", async () => {
-    await delegatedStaking.register_validator({ amount: 10 });
-    console.log(await getMainStakingState());
-  });
-
-  it("MainStaking: reward", async () => {
-    await mainStaking.reward(
-      delegatedStaking.$options.address.replace("ct_", "ak_"),
-      {
-        amount: 1000,
-      },
-    );
+  it("MainStaking: add_rewards", async () => {
+    const epoch = await hcElection
+      .epoch()
+      .then(({ decodedResult }) => decodedResult);
+    await mainStaking.add_rewards(epoch, [[validator.publicKey, 1000]], {
+      amount: 1000,
+    });
 
     console.log(await getMainStakingState());
   });
 
   it("DelegatedStaking: stake", async () => {
-    console.log(await getDelegatedStakingState());
-    await delegatedStaking.stake({ amount: 100 });
-    console.log(await getDelegatedStakingState());
+    console.log("getDelegatedStakingState", await getDelegatedStakingState());
+    await delegatedStaking.delegate_stake({
+      amount: 100,
+      onAccount: utils.getDefaultAccounts()[1],
+    });
+    console.log("getDelegatedStakingState", await getDelegatedStakingState());
     await nextEpoch();
-    await delegatedStaking.stake({ amount: 100 });
+    await delegatedStaking.delegate_stake({
+      amount: 100,
+      onAccount: utils.getDefaultAccounts()[1],
+    });
 
-    console.log(await getDelegatedStakingState());
+    console.log("getDelegatedStakingState", await getDelegatedStakingState());
     await nextEpoch();
     console.log(
       await aeSdk.getBalance(
@@ -118,12 +128,75 @@ describe("StakingContract", () => {
       ),
     );
     await nextEpoch();
-    console.log(await getDelegatedStakingState());
-    await delegatedStaking.stake({ amount: 100 });
-    console.log(await getDelegatedStakingState());
+    console.log("getDelegatedStakingState", await getDelegatedStakingState());
+    await delegatedStaking.delegate_stake({
+      amount: 100,
+      onAccount: utils.getDefaultAccounts()[1],
+    });
+    console.log("getDelegatedStakingState", await getDelegatedStakingState());
     await nextEpoch();
-    await delegatedStaking.force_update_epoch_rewards();
-    console.log(await getDelegatedStakingState());
-    console.log(await getMainStakingState());
+    console.log("getDelegatedStakingState", await getDelegatedStakingState());
+    console.log("getMainStakingState", await getMainStakingState());
+
+    for (const i of [...Array(20)]) {
+      await nextEpoch();
+    }
+    const epoch = await hcElection
+      .epoch()
+      .then(({ decodedResult }) => decodedResult);
+    await mainStaking.add_rewards(epoch, [[validator.publicKey, 10000]], {
+      amount: 10000,
+    });
+  });
+
+  it("DelegatedStaking: request_unstake_delegated_stakes", async () => {
+    for (const i of [...Array(20)]) {
+      await nextEpoch();
+    }
+    await delegatedStaking.request_unstake_delegated_stakes({
+      onAccount: utils.getDefaultAccounts()[1],
+    });
+    console.log(
+      "getDelegatedStakingState",
+      util.inspect(
+        await getDelegatedStakingState(),
+        false,
+        null,
+        true /* enable colors */,
+      ),
+    );
+  });
+
+  it("DelegatedStaking: withdraw", async () => {
+    for (const i of [...Array(20)]) {
+      await nextEpoch();
+    }
+    await delegatedStaking.withdraw({
+      onAccount: utils.getDefaultAccounts()[1],
+    });
+    console.log(
+      "getDelegatedStakingState",
+      util.inspect(
+        await getDelegatedStakingState(),
+        false,
+        null,
+        true /* enable colors */,
+      ),
+    );
+  });
+
+  it("DelegatedStaking: request_withdraw_rewards", async () => {
+    await delegatedStaking.request_withdraw_rewards({
+      onAccount: utils.getDefaultAccounts()[1],
+    });
+    console.log(
+      "getDelegatedStakingState",
+      util.inspect(
+        await getDelegatedStakingState(),
+        false,
+        null,
+        true /* enable colors */,
+      ),
+    );
   });
 });
